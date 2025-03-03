c7e6c3da3233fa16e071577666073fbef0deec5a
peppeborak <peppe@borak.se>
2025-03-03 19:57:38 +0100

    feat: camera works

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from 'react-native-vision-camera'
 
 export default function Index() {
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device = useCameraDevice('back')
+
+  React.useEffect(() => {
+    requestPermission()
+  }, [requestPermission])
+
+  if (!hasPermission) return
+  if (device == null) return
+
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Camera device={device} style={StyleSheet.absoluteFill} isActive={true} />
     </View>
-  );
+  )
 }
